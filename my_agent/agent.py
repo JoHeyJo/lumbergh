f85d19dab7d7b2da0cb4669e059e35642054adc8
jf <jpf0628@gmail.com>
2026-09-11 17:25:22 -0700

    Added file read capability along with resume txt file

--- my_agent/agent.py
+++ my_agent/agent.py
@@ -1,5 +1,5 @@
 from strands import Agent, tool
-from strands_tools import calculator, current_time
+from strands_tools import file_read
 
 
 # Define a custom tool as a Python function using the @tool decorator
@@ -26,14 +26,10 @@ def letter_counter(word: str, letter: str) -> int:
 
 # Create an agent with tools from the community-driven strands-tools package
 # as well as our custom letter_counter tool
-agent = Agent(tools=[calculator, current_time, letter_counter])
+agent = Agent(tools=[file_read])
 
 # Ask the agent a question that uses the available tools
 message = """
-I have 4 requests:
-
-1. What is the time right now?
-2. Calculate 3111696 / 74088
-3. Tell me how many letter R's are in the word "strawberry" 🍓
+Can you read the file at path "../Resume_Template.txt"
 """
 agent(message)
