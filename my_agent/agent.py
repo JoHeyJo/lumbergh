5bf1dd32298c844c916b22ad9f9191b9f461b8f5
jf <jpf0628@gmail.com>
2026-09-15 15:39:11 -0700

    Agent connecte with exa

--- my_agent/agent.py
+++ my_agent/agent.py
@@ -7,49 +7,6 @@ load_dotenv()
 
 import boto3
 
-runtime_arn_path = os.environ.get("RUNTIME_ARN_PATH")
-
-# Initialize the SSM client
-ssm_client = boto3.client("ssm", region_name=os.environ.get("REGION_NAME"))
-
-
-def get_agent_core_parameter(param_name):
-    try:
-        response = ssm_client.get_parameter(
-            Name=param_name,
-            WithDecryption=True,
-        )
-        return response["Parameter"]["Value"]
-    except ssm_client.exceptions.ParameterNotFound:
-        print(f"Error: Parameter '{param_name}' not found.")
-        return None
-
-
-
-# Example usage (Replace with your exact AgentCore SSM path)
-runtime_arn = get_agent_core_parameter(runtime_arn_path)
-
-# Define a custom tool as a Python function using the @tool decorator
-@tool
-def letter_counter(word: str, letter: str) -> int:
-    """
-    Count occurrences of a specific letter in a word.
-
-    Args:
-        word (str): The input word to search in
-        letter (str): The specific letter to count
-
-    Returns:
-        int: The number of occurrences of the letter in the word
-    """
-    if not isinstance(word, str) or not isinstance(letter, str):
-        return 0
-
-    if len(letter) != 1:
-        raise ValueError("The 'letter' parameter must be a single character")
-
-    return word.lower().count(letter.lower())
-
 
 # Create an agent with tools from the community-driven strands-tools package
 # as well as our custom letter_counter tool
@@ -57,6 +14,6 @@ agent = Agent(tools=[exa_search, exa_get_contents])
 
 # Ask the agent a question that uses the available tools
 message = """
-Can you find all recent software engineer roles posted on https://www.greenhouse.com/careers?
+Can you find all recent software engineer roles posted on https://www.greenhouse.com/?
 """
 agent(message)
