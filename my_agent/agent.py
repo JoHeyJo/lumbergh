ab1953e282366cef746f72a3bf2dd13e5026df93
jf <jpf0628@gmail.com>
2026-09-16 12:18:44 -0700

    Everythning seems to be working... NEXT: test exa - pull job listings from greenhouse

--- my_agent/agent.py
+++ my_agent/agent.py
@@ -4,13 +4,13 @@ from strands import Agent, tool
 from strands_tools import file_read
 from strands_tools.exa import exa_search, exa_get_contents
 from dotenv import load_dotenv
-import asyncio, os, aiohttp
 
 load_dotenv()
 
 
+agent = Agent(
+    system_prompt="Report tool failures rather than answering from memory",
+    tools=[exa_search, exa_get_contents],
+)
 
-
-agent = Agent(tools=[exa_search, exa_get_contents])
-
-agent("Search for 5 job listing websites")
+agent("Extract and summarize content of https://www.joannesfigueroa.com/")
