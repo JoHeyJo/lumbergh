c578e1aa5419cf02767b19ed9f49c14d7b353068
jf <jpf0628@gmail.com>
2026-09-17 12:11:54 -0700

    uninstall exa - try tavily this should offer better scarping capabilities of job listing websites

--- my_agent/agent.py
+++ my_agent/agent.py
@@ -7,19 +7,34 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# sites = {"indeed": None, "greenhouse": "https://job-boards.greenhouse.io/greenhouse"}
 
+# prompt = """
+# Extract and summarize software engineer roles at a Greenhouse. Try its ATS API first, then crawl
+#     its careers page. Return the public ATS API or None if none exists. Also, return 
+# """
+
+# prompt = """Extract and summarize 20 software engineer roles from this public ATS API:
+# https://boards-api.greenhouse.io/v1/boards/greenhouse/jobs?content=true
+#  """
+# prompt = """Extract and summarize top 5 software engineer roles from each:
+# Indeed, ZipRecruiter, my.greenhouse.io, https://hiringcafe.com/, Wellfound, CalJOBS, Jack and Jill. 
+#  """
+
+prompt = """Find public ATS API. Return object mapping company name
+endpoint. Only include those were you can query all their listings in a single
+request similar to Greenhouse.
+"""
+
+print(prompt)
 agent = Agent(
-    system_prompt="Report tool failures rather than answering from memory",
+    system_prompt="Report tool failures rather than answering from memory.",
     tools=[exa_search, exa_get_contents],
 )
 
-agent(
-    """Let's get the API endpoints that you can access for these job board sites:
-      career builder
-    """
-)
+agent(prompt)
 
-# Greenhouse: https://harvest.greenhouse.io/v1
+# Greenhouse: https://boards.greenhouse.io/greenhouse
 # Indeed: https://api.indeed.com
 # Jack and Jill: None,
 # LinkedIn: needs login
