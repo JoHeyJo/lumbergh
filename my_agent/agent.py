39cfb25d539d272ae0d9cc183acdca830e244bcb
jf <jpf0628@gmail.com>
2026-09-16 17:05:26 -0700

    Test api endp points

--- my_agent/agent.py
+++ my_agent/agent.py
@@ -13,4 +13,14 @@ agent = Agent(
     tools=[exa_search, exa_get_contents],
 )
 
-agent("Extract and summarize content of https://www.joannesfigueroa.com/")
+agent(
+    """Let's get the API endpoints that you can access for these job board sites:
+      career builder
+    """
+)
+
+# Greenhouse: https://harvest.greenhouse.io/v1
+# Indeed: https://api.indeed.com
+# Jack and Jill: None,
+# LinkedIn: needs login
+# Ashby:  https://api.ashbyhq.com/posting-api/job-board/{JOB_BOARD_NAME}
